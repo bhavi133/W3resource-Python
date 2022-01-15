2f71cfedcf6615149caf3a6aac4a1fa894da9e36
Bhavesh Ramesh Lulla <65904865+bhavi133@users.noreply.github.com>
2022-01-15 17:11:23 +0530

    Create Part-1.py

---
+++ Tuple/Part-1.py
@@ -0,0 +1,21 @@
+# 1. Write a Python program to create a tuple.
+
+tup1 = tuple(range(1, 6))
+print(tup1)
+print(type(tup1))
+
+tup2 = 1, 2, 3, 4, 5
+print(tup2)
+print(type(tup2))
+
+# 2. Write a Python program to create a tuple with different data types. 
+
+tup1 = (1, 2.0, "Red", True)
+print(tup1)
+
+# 3. Write a Python program to create a tuple with numbers and print one item.
+
+tup1 = 5, 10, 15, 20, 25
+print(tup1)
+tup1 = 5,
+print(tup1)
