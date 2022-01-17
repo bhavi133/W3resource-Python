54dce53337983c63005824afeeb1ce1ab9104174
Bhavesh Ramesh Lulla <65904865+bhavi133@users.noreply.github.com>
2022-01-17 22:08:37 +0530

    Update Part-1.py

--- Tuple/Part-1.py
+++ Tuple/Part-1.py
@@ -19,3 +19,47 @@ tup1 = 5, 10, 15, 20, 25
 print(tup1)
 tup1 = 5,
 print(tup1)
+
+# 4. Write a Python program to unpack a tuple in several variables.
+
+tup1 = (2, 3, 5)
+a, b, c = tup1
+print(a)
+print(b)
+print(c)
+
+# 5. Write a Python program to add an item in a tuple.
+
+tup1 = (1, 2, 3, 4)
+print(tup1)
+tup1 = tup1 + (5,)
+print(tup1)
+
+tup1 = (1, 2, 3, 4)
+print(tup1)
+tup1 = tup1[0:2] + (0, 5) + tup1[2:]
+print(tup1)
+
+tup1 = (1, 2, 3, 4)
+print(tup1)
+list1 = list(tup1)
+list1.append(5)
+print(tuple(list1))
+
+# 6. Write a Python program to convert a tuple to a string.
+
+tup1 = ('e', 'x', 'e', 'r', 'c', 'i', 's', 'e', 's')
+print("".join(tup1))
+
+tup1 = ('e', 'x', 'e', 'r', 'c', 'i', 's', 'e', 's')
+string = ""
+for i in tup1:
+    string = string + i
+print(string)
+
+# 7. Write a Python program to get the 4th element and 4th element from last of a tuple.
+
+tup1 = ("w", 3, "r", "e", "s", "o", "u", "r", "c", "e")
+print(tup1)
+print(tup1[3])
+print(tup1[-4])
