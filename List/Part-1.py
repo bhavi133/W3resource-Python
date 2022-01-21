35c949a38ad98419bbfa44b3bb171285f10b92d7
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2022-01-21 19:38:13 +0530

    Update Part-1.py

--- List/Part-1.py
+++ List/Part-1.py
@@ -43,3 +43,47 @@ def max_num_in_list(list1):
 
 list1 = [1, 2, -8, 0]
 print(max_num_in_list(list1))
+
+# 5. Write a Python program to count the number of strings where the string length is 2 or more and the first and last character are same from a given list of strings.
+
+def match_words(list1):
+    ctr = 0
+    for i in list1:
+        if len(i) > 2 and i[0] == i[-1]:
+            ctr += 1
+    return ctr
+
+list1 = ['abc', 'xyz', 'aba', '1221']
+print(match_words(list1))
+
+# 6. Write a Python program to get a list, sorted in increasing order by the last element in each tuple from a given list of non-empty tuples.
+
+def func1(n):
+    return n[-1]
+
+def func2(list1):
+    list2 = [i for i in sorted(list1, key=func1)]
+    return list2
+
+list1 = [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+print(func2(list1))
+
+# 7. Write a Python program to remove duplicates from a list.
+
+def remove_duplicates(nums):
+    unique = []
+    for i in nums:
+        if i not in unique:
+            unique.append(i)
+    return unique
+
+nums = [10, 20, 30, 20, 10, 50, 60, 40, 80, 50, 40]
+print(remove_duplicates(nums))
+
+# 8. Write a Python program to check a list is empty or not.
+
+list1 = []
+if len(list1) < 1:
+    print("List is empty")
+else:
+    print("List is not empty")
