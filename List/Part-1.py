e5fd241a1cdec2fe73574db826c00857d5332157
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2022-02-11 17:52:05 +0530

    Update Part-1.py

--- List/Part-1.py
+++ List/Part-1.py
@@ -159,3 +159,20 @@ from itertools import permutations
 colors = ['red', 'white', 'black']
 colors = list(permutations(colors))
 print(colors)
+
+# 19. Write a Python program to get the difference between the two lists.
+
+def difference_list(l1, l2):
+    l3 = [i for i in l1 if i not in l2]
+    l4 = [i for i in l2 if i not in l1]
+    return l3 + l4
+
+l1 = [1, 3, 5, 7, 9]
+l2 = [1, 2, 4, 6, 7, 8]
+print(difference_list(l1, l2))
+
+# 20. Write a Python program access the index of a list.
+
+nums = [5, 10, 15, 25, 35]
+for i, j in enumerate(nums):
+    print(i, j)
